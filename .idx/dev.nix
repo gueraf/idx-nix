ea8d3e5c76507231a4e3bc031cb1118c51c22f86
Fabian Güra <gyro@google.com>
2024-05-11 15:19:27 +0200

    Disable preview.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -50,24 +50,24 @@
   ];
 
   # Enable previews and customize configuration
-  idx.previews = {
-    enable = true;
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "start"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-          "--disable-host-check"
-        ];
-        manager = "web";
-      };
-    };
-  };
+  # idx.previews = {
+  #   enable = true;
+  #   previews = {
+  #     web = {
+  #       command = [
+  #         "npm"
+  #         "run"
+  #         "start"
+  #         "--"
+  #         "--port"
+  #         "$PORT"
+  #         "--host"
+  #         "0.0.0.0"
+  #         "--disable-host-check"
+  #       ];
+  #       manager = "web";
+  #     };
+  #   };
+  # };
 }
 
